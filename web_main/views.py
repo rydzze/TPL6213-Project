f469ec70c9d64df650c3113a09ec9551000c9f06
Muhammad Ariff Ridzlan <rydzze@gmail.com>
2023-11-16 14:19:40 +0800

    update views.py

--- web_main/views.py
+++ web_main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(response):
-    return HttpResponse("<h1>Hosting my web using Django!</h1>")
+    return HttpResponse("<h1>Hosting my web using Django!</h1> <p>This is home directory, add '/v1' behind the link to open v1 directory</p>")
 
 def v1(response):
     return HttpResponse("<h1>View 1</h1>")
